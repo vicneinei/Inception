7605d4a1168a0df214141637ed78e06d64f56eee
Victor Neirinckx <vneirinc@fu-r9-p4.s19.be>
2022-02-10 13:24:58 +0100

    Add wp user

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -14,9 +14,10 @@
 if [[ ! -f /var/www/wordpress/wp-config.php ]]; then
   mv /wp-config.php $WP_DIR;
   wp core download --version=5.9 --path=$WP_DIR;
-  wp core install --path=$WP_DIR --url=$URL --title=$TITLE --admin_user=$WP_USER --admin_password=$WP_PWD --admin_email=$WP_MAIL
+  wp core install --path=$WP_DIR --url=$URL --title=$TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_MAIL
   wp plugin install redis-cache --activate --path=$WP_DIR;
   wp plugin update --all --path=$WP_DIR;
   wp redis enable --path=$WP_DIR;
+  wp user create --path=$WP_DIR $WP_USER $WP_USER_MAIL --user_pass=$WP_USER_PWD;
 fi
 php-fpm8 -F;
